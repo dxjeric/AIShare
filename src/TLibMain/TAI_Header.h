a761b22fe6cb54790c42bdad959ec3b5b35a9dcf
Eric-Dang <frederick.dang@gmail.com>
2015-04-24 15:33:18 +0800

    add AI Share Code

---
+++ src/TLibMain/TAI_Header.h
@@ -0,0 +1,7 @@
+#ifndef __TAI_HEADER__
+#define __TAI_HEADER__
+
+#include "TAI_RefValue.h"
+#include "TAI_BevTree.h"
+
+#endif
---
+++ src/TLibMain/TUtility_Header.h
@@ -0,0 +1,10 @@
+#ifndef __TUTILITY_HEADER__
+#define __TUTILITY_HEADER__
+
+#include "TUtility_Array.h"
+#include "TUtility_List.h"
+#include "TUtility_MemPool.h"
+#include "TUtility_BitArray.h"
+#include "TUtility_AnyData.h"
+
+#endif
